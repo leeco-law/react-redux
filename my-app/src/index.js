d39920e069223ae451795421320f86cf5b019195
wb-llk329722 <wb-llk329722@alibaba-inc.com>
2018-04-12 17:08:10 +0800

    new

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './index/App';
+import createStore from './store/index';
 import registerServiceWorker from './registerServiceWorker';
+import reducers from './redecers/index';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers);
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
 registerServiceWorker();
---
+++ my-app/src/index/store/index.js
@@ -0,0 +1,6 @@
+import { createStore } from 'redux';
+import * as actions from './actions/index';
+let store = (reducers) => {
+    return {};
+};
+export default store;
